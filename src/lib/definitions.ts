c4b743435aa0d0720c2ce987d1ee59f22af494f8
Jacobtread <jacobtread@gmail.com>
2023-12-07 16:05:40 +1300

    Initial source

---
+++ src/lib/definitions.ts
@@ -0,0 +1,4 @@
+
+/// The number of skill points awarded for each class level
+export const POINTS_AT_LEVEL: number[] = [2, 5, 8, 11, 14, 17, 20, 24, 28, 32, 36, 40, 45, 50, 55, 60, 66, 72, 78, 84];
+
